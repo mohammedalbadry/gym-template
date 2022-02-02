21e831f8913de952dad9b54f187ea1c76da0cd3e
mohammed albadry <mohammed.albadry.dev@gmail.com>
2022-02-02 13:02:02 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.featured_slidshow').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.testimonial_slidshow').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
